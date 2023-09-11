938e37b640f4fafb6233ab4eb2cb3d99e1d34af8
DESKTOP-FSB4P98\s4ika <wabudabulaptap@gmail.com>
2023-09-11 21:49:26 +0300

    Add "swap neighbour" task

--- py_gen/main.py
+++ py_gen/main.py
@@ -1,10 +1,12 @@
 import josephus_problem, coordinate_quarters, more_than_previous
+import swap_neighbour
 
 
 def tasks():
     print("1. Задача Иосифа Флавия")
     print("2. Координатные четверти")
     print("3. Больше чем предыдущее")
+    print("4. Назад, вперёд и наоборот")
 
 
 def choice():
@@ -17,6 +19,8 @@ def choice():
             coordinate_quarters.coordinate_qtrs()
         case 3:
             more_than_previous.count_digits_more_than_prev()
+        case 4:
+            swap_neighbour.swap_nghbr()
         case _:
             print("Такой задачи нет =(")
 
