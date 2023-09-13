de8b7991f8cf62046eb5626141dfbae4c4ae49f4
DESKTOP-FSB4P98\s4ika <wabudabulaptap@gmail.com>
2023-09-14 00:57:45 +0300

    Add chunky task

--- py_gen/main.py
+++ py_gen/main.py
@@ -1,6 +1,6 @@
 import josephus_problem, coordinate_quarters, more_than_previous, swap_neighbour,\
     developmental_shift, various_elements, mul_dig, rock_paper_scissors,rock_paper_scissors_lizard_spok,\
-    o_and_p,sillicon_valley,roscomnadzor, pascal_triangle, packing_duplicates
+    o_and_p,sillicon_valley,roscomnadzor, pascal_triangle, packing_duplicates, chunky
 
 
 def tasks():
@@ -19,6 +19,7 @@ def tasks():
     print("13. Треугольник Паскаля (выводим n-й уровень)")
     print("14. Треугольник Паскаля")
     print("15. Упаковка дупликатов")
+    print("16. Разбиение на чанки")
 
 
 def choice():
@@ -55,6 +56,8 @@ def choice():
             pascal_triangle.pascal_triangle()
         case 15:
             packing_duplicates.pack()
+        case 16:
+            chunky.run()
         case _:
             print("Такой задачи нет =(")
 
